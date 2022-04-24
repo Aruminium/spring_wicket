2248d9e6e07c54db41916a1255efa5d0a8a8c532
Aruminium <b2201680@photon.chitose.ac.jp>
2022-04-25 00:08:09 +0900

    init

---
+++ src/main/java/com/example/wickettest/WicketTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.wickettest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WicketTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WicketTestApplication.class, args);
+    }
+
+}
