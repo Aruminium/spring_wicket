116e18de40abbfbadc14034bf97bbb4d073eb1f0
Aruminium <b2201680@photon.chitose.ac.jp>
2022-04-26 16:02:07 +0900

    work4 finish

--- src/main/java/com/example/wickettest/page/HomePage.java
+++ src/main/java/com/example/wickettest/page/HomePage.java
@@ -9,6 +9,7 @@ import com.example.wickettest.service.ISampleService;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 
+@WicketHomePage
 @MountPath("Home")
 public class HomePage extends WebPage{
 
--- src/main/java/com/example/wickettest/page/UserMakerPage.java
+++ src/main/java/com/example/wickettest/page/UserMakerPage.java
@@ -5,11 +5,40 @@ import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wicketstuff.annotation.mount.MountPath;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.Model;
 
 @MountPath("UserMaker")
 public class UserMakerPage extends WebPage{
     public UserMakerPage() {
-        var toHomeLink = new BookmarkablePageLink("toHome", HomePage.class);
+        var userNameModel = Model.of("");
+        var userPassModel = Model.of("");
+
+        var toHomeLink = new BookmarkablePageLink<>("toHome", HomePage.class);
         add(toHomeLink);
+
+        var userInfoForm = new Form<Void>("userInfo"){
+            @Override
+            protected void onSubmit(){
+                String userName = userNameModel.getObject();
+                String userPass = userPassModel.getObject();
+                String msg = "送信データ:"
+                        + userName
+                        + ","
+                        + userPass;
+                System.out.println(msg);
+                setResponsePage(new UserMakerCompPage(userNameModel));
+            }
+        };
+        add(userInfoForm);
+
+        var userNameField = new TextField<>("userName", userNameModel);
+        userInfoForm.add(userNameField);
+
+        var userPassField = new PasswordTextField("userPass", userPassModel);
+        userInfoForm.add(userPassField);
+        add(userInfoForm);
     }
 }
