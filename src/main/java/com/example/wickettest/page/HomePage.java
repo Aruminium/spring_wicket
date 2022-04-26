73714f5e9efa9dee69f3b5c84f47ffa0e72eadb8
Aruminium <b2201680@photon.chitose.ac.jp>
2022-04-26 12:49:45 +0900

    work2 finish

--- src/main/java/com/example/wickettest/page/HomePage.java
+++ src/main/java/com/example/wickettest/page/HomePage.java
@@ -5,11 +5,15 @@ import org.apache.wicket.markup.html.WebPage;
 import org.wicketstuff.annotation.mount.MountPath;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.Model;
+import com.example.wickettest.service.ISampleService;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
 @WicketHomePage
 @MountPath("Home")
 public class HomePage extends WebPage{
 
+    @SpringBean
+    private ISampleService service;
     public HomePage(){
         var youModel = Model.of("Wicket-Spring-Boot");
         var gakusekiModel = Model.of("b2201680");
@@ -17,8 +21,14 @@ public class HomePage extends WebPage{
         var gakusekiLabel = new Label("gakuseki", gakusekiModel);
         var nameModel = Model.of("寺山碧威");
         var nameLabel = new Label("name", nameModel);
+        var timeModel = Model.of(service.makeCurrentHMS());
+        var timeLabel = new Label("time", timeModel);
+        var randModel = Model.of(service.makeRandint());
+        var randLabel = new Label("rand", randModel);
+        add(timeLabel);
         add(nameLabel);
         add(gakusekiLabel);
         add(youLabel);
+        add(randLabel);
     }
 }
