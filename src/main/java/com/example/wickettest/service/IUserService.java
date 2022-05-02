69ce28bc136c73a76f1febeb099b9f7b2d0f1246
Aruminium <b2201680@photon.chitose.ac.jp>
2022-05-02 14:41:00 +0900

    finish

--- src/main/java/com/example/wickettest/service/IUserService.java
+++ src/main/java/com/example/wickettest/service/IUserService.java
@@ -2,4 +2,13 @@ package com.example.wickettest.service;
 
 public interface IUserService {
     public void registerUser(String userName, String userPass);
+
+    /**
+     * ユーザ名とパスワードをデータベースに照合する
+     *
+     * @param userName ユーザ名
+     * @param userPass パスワード
+     * @return 照合成功であれば<code>true</code>, 照合失敗は<code>false</code>
+     */
+    public boolean existsUser(String userName, String userPass);
 }
